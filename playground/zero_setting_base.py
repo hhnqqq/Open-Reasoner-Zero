1b0e6c0f4b0913fa2d40be1a282a34c70af316ec
hhnqqq <895228612@qq.com>
2025-03-16 22:43:19 +0800

    修正chat template，使其能够适配热启动模型

--- playground/zero_setting_base.py
+++ playground/zero_setting_base.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from jinja2 import Template
-
 from orz.ppo import PromptDataset
 
 
@@ -10,29 +9,26 @@ class CustomDataset(PromptDataset):
         super().__init__(*args, **kwargs)
 
     def process_dialogue(self, dialogue: List):
-        prompt_template_jinja = """\
-{{bos_token}}A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer. \
-The reasoning process is enclosed within <think> </think> and answer is enclosed within <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>. User: {{prompt}}
-Assistant: <think>\
-"""
+        system_prompt = """A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer. \
+The reasoning process is enclosed within <think> </think> and answer is enclosed within <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>. """
+
         prompt_instruction_template_jinja = """\
-You must put your answer inside <answer> </answer> tags, i.e., <answer> answer here </answer>. And your final answer will be extracted automatically by the \\boxed{} tag.
-This is the problem:
-{{prompt}}
+Please reason step by step, and put your final answer within \\boxed{} for closed-end problem (including multiple choice problem).
+If the problem requires multiple answers to be answered, place all final answers in one \\boxed{} environment, separated by commas
+If the problem is in Chinese, provide your reasoning and answer in Chinese. Otherwise, use English.
+Problem: {{prompt}}
 """
 
-        assert len(dialogue) == 2, "dialogue must contain 2 items"
-
-        prompt_instruction_template = Template(prompt_instruction_template_jinja)
-        prompt_instruction = prompt_instruction_template.render(prompt=dialogue[0]["value"])
-        prompt_template = Template(prompt_template_jinja)
-        if self.tokenizer.bos_token_id is None:
-            bos_token = ""
-        else:
-            bos_token = self.tokenizer.decode([self.tokenizer.bos_token_id])
-        prompt = prompt_template.render(bos_token=bos_token, prompt=prompt_instruction)
-
-        extra = {"answer": dialogue[1]["ground_truth"]["value"]}
+        instruction_template = Template(prompt_instruction_template_jinja)
+        instruction = instruction_template.render(prompt=dialogue["problem"])
+        system_content = {"content": system_prompt, "role":"system"}
+        input_content = {"content": instruction, "role": "user"}
+        messages = [system_content,input_content]
+        prompt = self.tokenizer.apply_chat_template(messages,
+                                                    tokenize=False,
+                                                    add_generation_prompt=True)
+        # A lot of solutions are None.
+        extra = {"answer": dialogue["answer"], "solution": dialogue["solution"] if "solution" in dialogue.keys() else " "}
 
         return prompt, extra
 
@@ -42,30 +38,29 @@ class EvalCustomDataset(PromptDataset):
         super().__init__(*args, **kwargs)
 
     def process_dialogue(self, dialogue: dict):
-        prompt_template_jinja = """\
-{{bos_token}}A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer. \
-The reasoning process is enclosed within <think> </think> and answer is enclosed within <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>. User: {{prompt}}
-Assistant: <think>\
-"""
+        system_prompt = """A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer. \
+The reasoning process is enclosed within <think> </think> and answer is enclosed within <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>. """
+
         prompt_instruction_template_jinja = """\
-You must put your answer inside <answer> </answer> tags, i.e., <answer> answer here </answer>. And your final answer will be extracted automatically by the \\boxed{} tag.
-This is the problem:
-{{prompt}}
+Please reason step by step, and put your final answer within \\boxed{} for closed-end problem (including multiple choice problem).
+If the problem requires multiple answers to be answered, place all final answers in one \\boxed{} environment, separated by commas
+If the problem is in Chinese, provide your reasoning and answer in Chinese. Otherwise, use English.
+Problem: {{prompt}}
 """
         assert isinstance(dialogue, dict), "dialogue must be a dict"
-        assert "prompt" in dialogue, "dialogue must contain prompt"
-        assert "final_answer" in dialogue, "dialogue must contain final_answer"
+        assert "problem" in dialogue, "dialogue must contain problem"
+        assert "answer" in dialogue, "dialogue must contain answer"
         assert "file_name" in dialogue, "dialogue must contain file_name"
 
-        prompt_instruction_template = Template(prompt_instruction_template_jinja)
-        prompt_instruction = prompt_instruction_template.render(prompt=dialogue["prompt"][0]["value"])
-        prompt_template = Template(prompt_template_jinja)
-        if self.tokenizer.bos_token_id is None:
-            bos_token = ""
-        else:
-            bos_token = self.tokenizer.decode([self.tokenizer.bos_token_id])
-        prompt = prompt_template.render(bos_token=bos_token, prompt=prompt_instruction)
+        instruction_template = Template(prompt_instruction_template_jinja)
+        instruction = instruction_template.render(prompt=dialogue["problem"])
+        system_content = {"content": system_prompt, "role":"system"}
+        input_content = {"content": instruction, "role": "user"}
+        messages = [system_content,input_content]
+        prompt = self.tokenizer.apply_chat_template(messages,
+                                                    tokenize=False,
+                                                    add_generation_prompt=True)
 
-        extra = {"answer": dialogue["final_answer"], "file_name": dialogue["file_name"]}
+        extra = {"answer": dialogue["answer"], "file_name": dialogue["file_name"]}
 
         return prompt, extra
